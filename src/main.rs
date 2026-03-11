0aaf2e4725213e9021a2c325af29a900b59c2b79
hunterd129 <hunterd129@icloud.com>
2026-03-11 09:10:02 -0600

    modified main.rs to set the file paths as 'path\to\your\wallpaper'

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,9 @@ use windows::Win32::UI::WindowsAndMessaging::{
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // 1. The folders from your PS1 script
     let folders = vec![
-        r"C:\Users\hunte\Pictures\Wallpapers\Architecture Wallpapers",
-        r"C:\Users\hunte\Pictures\Wallpapers\Libadwaita Wallpapers",
-        r"C:\Users\hunte\Pictures\Wallpapers\Yuzusoft Wallpapers",
+        r"path\to\your\wallpapers",
+//      r"path\to\your\wallpapers",
+//      r"path\to\your\wallpapers",
     ];
 
     let mut rng = rand::thread_rng();
