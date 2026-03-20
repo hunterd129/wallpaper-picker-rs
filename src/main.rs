14035a405e2b430d1e43bf0eb900e9f7a75a53fc
hunterd129 <hunterd129@icloud.com>
2026-03-19 19:33:32 -0600

    reconfigured for GNOME desktop

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,60 @@
-#![windows_subsystem = "windows"]
 use std::fs;
 use std::path::PathBuf;
 use rand::seq::SliceRandom;
-use std::os::windows::ffi::OsStrExt;
-use std::ffi::OsStr;
-use windows::Win32::UI::WindowsAndMessaging::{
-    SystemParametersInfoW, SPI_SETDESKWALLPAPER, SPIF_UPDATEINIFILE,
-};
+use gio::prelude::*;
+use gio::Settings;
+use notify_rust::Notification;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // - define which folder(s) to use
-    let folders = vec![
-        r"path\to\your\wallpapers",
-//      r"path\to\your\wallpapers", *add your own paths if you want multiple folders being used~*
-//      r"path\to\your\wallpapers", *add your own paths if you want multiple folders being used~* 
-    ];
+    //Updated file paths for POSIX systems
+let folders = vec![
+    "/home/hunter/Pictures/Wallpapers/Architecture Wallpapers",
+    "/home/hunter/Pictures/Wallpapers/Libadwaita Wallpapers",
+    "/home/hunter/Pictures/Wallpapers/Yuzusoft Wallpapers",
+];
 
-    let mut rng = rand::thread_rng();
+let mut rng = rand::thread_rng();
 
-    // - Pick a random folder
-    let selected_folder = folders.choose(&mut rng).expect("Folder list is empty");
+//Pick a random folder
+let selected_folder = folders.choose(&mut rng).ok_or("folder list is empty")?;
 
-    // - Get all files and pick a random one
-    let entries = fs::read_dir(selected_folder)?
-        .filter_map(|res| res.ok())
-        .map(|e| e.path())
-        .filter(|path| path.is_file())
-        .collect::<Vec<PathBuf>>();
+//collect files
+let entries = fs::read_dir(selected_folder)?
+    .filter_map(|res| re.ok())
+    .map(|e| e.path())
+    .filter(|path| path.is_file())
+    .collect::<Vec<PathBuf>>();
 
-    let selected_wallpaper = entries.choose(&mut rng).ok_or("No files found in folder")?;
-
-    // - Convert the path to the "Wide String" format user32.dll expects
-    let path_wide: Vec<u16> = OsStr::new(selected_wallpaper.as_os_str())
-        .encode_wide()
-        .chain(std::iter::once(0)) // Null terminator
-        .collect();
-
-    //  - set wallpaper
-    unsafe {
-        SystemParametersInfoW(
-            SPI_SETDESKWALLPAPER,
-            0,
-            Some(path_wide.as_ptr() as *mut _),
-            SPIF_UPDATEINIFILE,
-        )?;
+    if entries.is_empty() {
+        return Err("No files found in the selected folder".into());
     }
 
-    println!("Success! Wallpaper set to: {:?}", selected_wallpaper);
-    Ok(())
+//pick the wallpaper
+let selected_wallpaper = entries.choose(&mut rng).unwrap();
+let wallpaper_path = selected_wallpaper.to_str().ok_or("Invalid path")?;
+
+let wallpaper_uri = format!("file//{}", wallpaper_path);
+
+//set wallpaper via gsettings
+let settings = Settings::new("org.gnome.desktop.background");
+settings.set_string("picture-uri", &wallpaper_uri)?;
+settings.set_string("picture-uri-dark", &wallpaper_uri)?;
+
+//apply changes
+settings.apply();
+
+//send notification
+let file_name = selected_wallpaper
+    .file_name()
+    .and_then(|n| n.to_str())
+    .unwrap_or("Unknown Image");
+
+Notification::new()
+    .summary("Wallpaper Updated")
+    .body(&format!("New look: {}", file_name))
+    .appname("wallpaperPicker")
+    .timeout(5000)
+    .show()?;
+
+ok(())
 }
